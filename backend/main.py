7b95fb20df490510a73df5705672684f7a461c81
HeraldOfCalamity <releegva@gmail.com>
2023-10-10 19:46:04 -0400

    in progress: implementation of different processing algorithms

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from Process import ProcessItem
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import ValidationError
-
+from ProcessManagement import ProcessManagement
 
 app = FastAPI()
 
@@ -25,11 +25,23 @@ def read_root():
 
 @app.get('/api/process')
 def get_all_process():
-    response = []
-    for process in processArr:
-        response.append(ProcessItem(**process))
+    data = [
+        ProcessItem(**{'name':'A','cpu_time':4,'arrival':0}),
+        ProcessItem(**{'name':'B','cpu_time':3,'arrival':1}),
+        ProcessItem(**{'name':'C','cpu_time':7,'arrival':2}),
+    ]
 
-    return response
+    # data = []
+    # for item in incoming:
+    #     data.append(ProcessItem(**item))
+
+    
+    sol = ProcessManagement(data, 'FCFS')
+    a = sol.generateGantt()
+    print(a)
+    
+    # print(sol.data)
+    return a
 
 @app.post('/api/process')
 def create_process_array(pList: List[ProcessItem]):
